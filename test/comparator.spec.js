27d24c9c84f82504a19a45ad7fe3bfd1b22d5eeb
Marton Hipszki <mhipszki@gmail.com>
2015-10-25 12:58:06 +0000

    symbol comparator's outcome is 1 when the first symbol beats the second

--- test/comparator.spec.js
+++ test/comparator.spec.js
@@ -1,6 +1,15 @@
 'use strict';
 
-function compare () {
+var rules = {
+	'A': {
+		beats: ['B']
+	}
+};
+
+function compare (symbolOne, symbolTwo) {
+	if (rules[symbolOne].beats.indexOf(symbolTwo) > -1) {
+		return 1;
+	}
 	return 0;
 }
 
@@ -11,6 +20,19 @@ describe('symbol comparator', function () {
 		it('should return 0', function () {
 			var outcome = compare('A', 'A');
 			expect(outcome).to.equal(0);
+		});
+
+	});
+
+	describe('when provided with different symbols', function () {
+
+		describe('and the 1st symbol beats the 2nd', function () {
+
+			it('should return 1', function () {
+				var outcome = compare('A', 'B');
+				expect(outcome).to.equal(1);
+			});
+
 		});
 
 	});
