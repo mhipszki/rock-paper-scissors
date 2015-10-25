cc50db6fd262c821929e1d466d29053db304f9c4
Marton Hipszki <mhipszki@gmail.com>
2015-10-25 14:23:24 +0000

    rules must contain at least one rule

--- test/rules-validator.spec.js
+++ test/rules-validator.spec.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var errors = {
-	invalidType: 'rules must be an object'
+	invalidType: 'rules must be an object',
+	noRulesDefined: 'rules must contain at least one rule'
 };
 
 function validate (rules) {
 	if (typeof rules !== 'object') {
 		throw new Error(errors.invalidType);
+	}
+	if (Object.keys(rules).length === 0) {
+		throw new Error(errors.noRulesDefined);
 	}
 	return true;
 }
@@ -16,7 +20,7 @@ describe('rules validator', function () {
 	describe('when provided with a valid rules object', function () {
 
 		it('should return true', function () {
-			var rules = {};
+			var rules = { a: 'rule' };
 			var result = validate(rules);
 			expect(result).to.be.true;
 		});
@@ -31,6 +35,14 @@ describe('rules validator', function () {
 				return validate(rules);
 			}
 			expect(validation).to.throw(errors.invalidType);
+		});
+
+		it('must contain at least one rule', function () {
+			function validation () {
+				var rules = {};
+				return validate(rules);
+			}
+			expect(validation).to.throw(errors.noRulesDefined);
 		});
 
 	});
