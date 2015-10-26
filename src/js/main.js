e312364d19c6283084f409395b89d3697f2b7ff7
Marton Hipszki <mhipszki@gmail.com>
2015-10-26 00:04:28 +0000

    example games now use computer opponent

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var generateGameWith = require('./game-factory');
+var generateComputerOpponentWith = require('./computer-factory');
 
 var symbolDefinitions = [
 	{
@@ -30,6 +31,18 @@ function play (s1, s2) {
 	console.log(s1, s2, '=>', game.message());
 }
 
+function playAgainstComputer (s1) {
+	var game = generateGameWith(symbolDefinitions);
+	var computer = generateComputerOpponentWith(symbolDefinitions);
+	var s2 = computer.chooseSymbol();
+	game.play(s1, s2);
+	console.log(s1, s2, '=>', game.message());
+}
+
 play('rock', 'scissors');
 play('rock', 'paper');
 play('rock', 'rock');
+
+playAgainstComputer('scissors');
+playAgainstComputer('scissors');
+playAgainstComputer('scissors');
