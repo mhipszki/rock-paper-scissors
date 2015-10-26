7648d04228d66c37664c62b009697a9a0377658b
Marton Hipszki <mhipszki@gmail.com>
2015-10-26 01:02:22 +0000

    added basic UI to show game outcome

--- TODOS.md
+++ TODOS.md
@@ -1,7 +1,5 @@
 # TODOS
 
-- add basic UI to show the game result
-
 - when computer is selected on UI instead of a symbol, play the game with computer vs computer
 
 - describe basic architecture in README
--- src/js/main.js
+++ src/js/main.js
@@ -25,15 +25,41 @@ var symbolDefinitions = [
 	}
 ];
 
+var select = document.querySelector('.available-symbols');
+
 function playAgainstComputer (s1) {
 	var game = generateGameWith(symbolDefinitions);
 	var computer = generateComputerOpponentWith(symbolDefinitions);
 	var s2 = computer.chooseSymbol();
+
 	game.play(s1, s2);
-	console.log(s1, s2, '=>', game.message());
+
+	var playerMessage;
+
+	switch (game.outcome()) {
+	case 1:
+		playerMessage = 'Congrats, you won! :)';
+		break;
+	case 2:
+		playerMessage = 'You lost :( better luck next time!';
+		break;
+	case 0:
+		playerMessage = 'It\'s a tie! Play again?';
+		break;
+	}
+
+	var outcome = document.querySelector('.outcome h1');
+	outcome.textContent = playerMessage;
+
+	var computerChoice = document.querySelector('.outcome h3');
+	computerChoice.textContent = 'The computer has chosen '+s2;
+
+	var message = document.querySelector('.outcome p');
+	message.textContent = game.message();
+
+	select.selectedIndex = 0;
 }
 
-var select = document.querySelector('.available-symbols');
 select.addEventListener('change', function (event) {
 	var chosenSymbol = event.target.value;
 	if (chosenSymbol !== 'null') {
